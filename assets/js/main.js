8883aa59661433f9b08e7a58953e65ac8b13f6cc
lekhrajshubhu <lekhraj@shubhu.com>
2024-11-09 21:06:57 +0545

    dashboard completed - one phase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 // ========== LEFT SIDEBAR JS ======= //
-$(document).on('click','#sidebar li',function(e) {
+$(document).on('click','#sidebar li a',function(e) {
     e.preventDefault(0);
-    $(this).addClass('active').siblings().removeClass('active');
+    $(document).find("#sidebar li a").removeClass('active-sidenav');
+    $(this).addClass('active-sidenav');
 })
 
 $(".sub-menu ul").hide();
 $(".sub-menu a").click(function() {
     // Close all other submenus except the current one
-    $(".sub-menu ul").not($(this).siblings("ul")).slideUp("100");
+    // $(".sub-menu ul").not($(this).siblings("ul")).slideUp("100");
     $(".sub-menu a").not(this).find(".right").removeClass("fa-caret-up").addClass("fa-caret-down");
     
     // Toggle the current submenu
